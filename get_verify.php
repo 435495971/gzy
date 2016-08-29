0b38939fc4b59748e6b083cdb35c7fb35322461a
shabby <435495971@qq.com>
2016-08-29 19:20:28 +0800

    2

--- get_verify.php
+++ get_verify.php
@@ -7,6 +7,6 @@ $cookie = $str[1];
 if(preg_match('/id="verifyDiv" style="display:none;"/iU',$result['content'],$str))
 	$img=get_img('http://jw.gzucm.edu.cn/',$cookie);
 else
-	$img=get_img('http://119.29.238.68/wechat/no_verify.png')
+	$img=get_img('http://119.29.238.68/wechat/no_verify.png');
 echo $img['content'];
 ?>
