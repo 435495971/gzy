ccc3015a89835e65316b04f6b44c87b96444f6f1
shabby <435495971@qq.com>
2016-08-29 19:38:31 +0800

    3

--- get_verify.php
+++ get_verify.php
@@ -4,8 +4,8 @@ session_start();
 $result=get_Web_Page('http://jw.gzucm.edu.cn/');
 preg_match('/Set-Cookie:(.*);/iU',$result['header'],$str);
 $cookie = $str[1];
-if(preg_match('/id="verifyDiv" style="display:none;"/iU',$result['content'],$str))
-	$img=get_img('http://jw.gzucm.edu.cn/',$cookie);
+if(!preg_match('/id="verifyDiv" style="display:none;"/iU',$result['content'],$str))
+	$img=get_img('http://jw.gzucm.edu.cn/yzm?d='.time(),$cookie);
 else
 	$img=get_img('http://119.29.238.68/wechat/no_verify.png');
 echo $img['content'];
