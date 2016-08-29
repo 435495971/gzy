c585bd2cbab0dbc62b8a6c6bc50960e584ea5d25
shabby <435495971@qq.com>
2016-08-29 20:59:57 +0800

    3

--- get_class.php
+++ get_class.php
@@ -5,16 +5,7 @@ $cookie=$_SESSION['cookie'];
 {
 	$url='http://210.38.108.23/xsbjkbcx!getKbRq.action?xnxqdm=20152&zc=1';
 	$result=get_Web_Page($url,$cookie);
-	$result=json_decode($result['content']);
+	echo $result['content'];
 }
-$tmp=array();
-$key=0;
-foreach ($result[0] as $value) {
-	$num=intval($value->xq)*100000000+intval($value->jcdm);
-	$tmp[$num]=$value;
-}
-ksort($tmp);
-$result[0]=$tmp;
-echo '</br></br>';
-echo json_encode($result,JSON_UNESCAPED_UNICODE);
+
 ?>
