3c52d300a6f2c27a8403daddaed5a1bccbb0a5eb
shabby <435495971@qq.com>
2016-08-29 20:52:22 +0800

    3

---
+++ get_class.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require('curl.php');
+$cookie=$_SESSION['cookie'];
+{
+	$url='http://210.38.108.23/xsbjkbcx!getKbRq.action?xnxqdm=20152&zc=1';
+	$result=get_Web_Page($url,$cookie);
+	$result=json_decode($result['content']);
+}
+$tmp=array();
+$key=0;
+foreach ($result[0] as $value) {
+	$num=intval($value->xq)*100000000+intval($value->jcdm);
+	$tmp[$num]=$value;
+}
+ksort($tmp);
+$tmp[]=$result[1]);
+echo '</br></br>';
+echo json_encode($tmp,JSON_UNESCAPED_UNICODE);
+?>
